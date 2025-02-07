df70bcd6937cec6cd9bdda0b5e2e6062f016074c
Grace Jung <gjung20@my.bcit.ca>
2025-02-07 13:56:36 -0800

    adding classes and methods

---
+++ src/code/BookStore.java
@@ -0,0 +1,106 @@
+/**
+ * Represents a BookStore object.
+ */
+public class BookStore
+{
+
+    /**
+     * Constructs a BookStore object.
+     */
+    public BookStore()
+    {
+
+    }
+
+    /**
+     * Print all titles in UPPERCASE.
+     */
+    void printAllTitles()
+    {
+
+    }
+
+    /**
+     * Prints all titles that contain the specified parameter.
+     * @param title
+     */
+    void printBookTitle(String title)
+    {
+
+    }
+
+    /**
+     * Prints all titles in alphabetical order, A-Z.
+     */
+    void printTitlesInAlphaOrder()
+    {
+
+    }
+
+    /**
+     * Prints all books for the inputted decade
+     * IE 2000 -> print all book titles from 2000 - 2009
+     * @param decade
+     */
+    void printGroupsByDecade(int decade)
+    {
+
+    }
+
+    /**
+     * Find the longest title in the bookstore.
+     */
+    void getLongest()
+    {
+
+    }
+
+    /**
+     * Returns true or false.
+     * @param year
+     * @return
+     */
+    boolean isThereBookWrittenIn(int year)
+    {
+        return false;
+    }
+
+    /**
+     * Returns the amount of Books that contain this word
+     * in their title.
+     * @param word
+     * @return
+     */
+    int howManyBooksContain(String word)
+    {
+        return 0;
+    }
+
+    /**
+     * What percentage of the books were written between
+     * these two years (inclusive)?
+     * @param first
+     * @param last
+     */
+    void whichPercentWrittenBetween(int first, int last)
+    {
+
+    }
+
+    /**
+     * Returns the oldest book.
+     */
+    void getOldestBook()
+    {
+
+    }
+
+    /**
+     * Returns the List of all books whose title is this length.
+     * @param titleLength
+     */
+    void getBooksThisLength(int titleLength)
+    {
+
+    }
+}
---
+++ src/code/Novel.java
@@ -0,0 +1,5 @@
+public class Novel {
+    String title;
+    String authorName;
+    int yearPublished;
+}
